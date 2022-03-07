aa209704aaa55e10f91fba04334975c682d64f64
yashpatil007 <yashpatil85326@gmail.com>
2022-03-07 11:43:48 +0530

    ADDED: Employee Wage calculation for Institute'

---
+++ src/com/infogalaxy/EmployeeWageCalculation.java
@@ -0,0 +1,25 @@
+package com.infogalaxy;
+
+import java.util.Random;
+
+public class EmployeeWageCalculation {
+    public static void main(String[] args) {
+        //Welcome massage for user
+        System.out.println("Welcome Employee Wage calculation");
+        //UC-2 Calculation Daily Wage
+        final int WAGE_PER_HR = 20;
+        final int FULL_DAY_HR = 8;
+        int total_Wage = 0;
+        //UC-1 Employee Attendance
+        Random random = new Random();
+        int empAttendance = random.nextInt()%2;
+        System.out.println(empAttendance);
+        if(empAttendance == 0){
+            System.out.println("Employee id Absent");
+        } else {
+            System.out.println("Employee is Present");
+            total_Wage = WAGE_PER_HR * FULL_DAY_HR;
+        }
+        System.out.println("Total Wage="+total_Wage);
+    }
+}
