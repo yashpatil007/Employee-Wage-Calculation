984320875d649d6f6edcf8c43ba08a7c6158e3d0
yashpatil007 <yashpatil85326@gmail.com>
2022-03-08 10:27:30 +0530

    MODIFIED: Employee Wage Calculation

--- src/com/infogalaxy/EmployeeWageCalculation.java
+++ src/com/infogalaxy/EmployeeWageCalculation.java
@@ -7,18 +7,31 @@ public class EmployeeWageCalculation {
         //Welcome massage for user
         System.out.println("Welcome Employee Wage calculation");
         //UC-2 Calculation Daily Wage
-        final int WAGE_PER_HR = 20;
+        final int PART_TIME_WAGE = 4;
         final int FULL_DAY_HR = 8;
+        final int WAGE_PER_HR = 20;
         int total_Wage = 0;
         //UC-1 Employee Attendance
         Random random = new Random();
         int empAttendance = random.nextInt()%2;
         System.out.println(empAttendance);
         if(empAttendance == 0){
-            System.out.println("Employee id Absent");
+            System.out.println("Employee is Absent");
         } else {
             System.out.println("Employee is Present");
-            total_Wage = WAGE_PER_HR * FULL_DAY_HR;
+            int empType = random.nextInt() %2;
+            System.out.println(empType);
+            switch(empType)
+            {
+                case 0:
+                    System.out.println("Employee is on Part Time");
+                    total_Wage = WAGE_PER_HR * PART_TIME_WAGE;
+                    break;
+                case 1:
+                    System.out.println("Employee is On Full Time");
+                    total_Wage = WAGE_PER_HR * FULL_DAY_HR;
+                    break;
+            }
         }
         System.out.println("Total Wage="+total_Wage);
     }
